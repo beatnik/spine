d7faf3d46975a0a7b9f26d16b669f2abcf8bbe27
beatnik <beatnik@7e66c766-c70f-4bca-b50f-0f7bb70c072f>
2006-05-26 20:50:31 +0000

    Reverting back to revision 51
    git-svn-id: svn+ssh://192.168.0.197/Users/hendrik/Repositories@54 7e66c766-c70f-4bca-b50f-0f7bb70c072f

--- Spine/lib/Apache/SPINE/Index.pm
+++ Spine/lib/Apache/SPINE/Index.pm
@@ -67,10 +67,7 @@ sub handler
   my $file = $r->filename; 
   my $uri = $r->uri; 
   my $dbh = undef;
-
-  ### check for file-on-disk
-  return DECLINED if (length $uri > 1 and -e $file);
-
+  if ($r->uri && -e $r->document_root.$uri && $page ne '/') { return DECLINED; }
   #We pretend to know how to handle files that actually exist!!
   if (!$dbh) { $dbh = &initialise($r); }
   my $cookie = undef;
