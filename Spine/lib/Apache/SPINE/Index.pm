397290fea24af677345e23c85ee92cae7b91307c
beatnik <beatnik@7e66c766-c70f-4bca-b50f-0f7bb70c072f>
2006-06-24 17:16:14 +0000

    Fixed / problem
    git-svn-id: svn+ssh://192.168.0.197/Users/hendrik/Repositories@88 7e66c766-c70f-4bca-b50f-0f7bb70c072f

--- Spine/lib/Apache/SPINE/Index.pm
+++ Spine/lib/Apache/SPINE/Index.pm
@@ -67,7 +67,8 @@ sub handler
   my $file = $r->filename; 
   my $uri = $r->uri; 
   my $dbh = undef;
-  if ($r->uri && -e $r->document_root.$uri && $page ne '/') { return DECLINED; }
+  if ($r->uri and -e $r->document_root.$uri and $page ne '/' and $location ne "/") { return DECLINED; }
+  if (length($uri) > 1 and -e $file and $location eq "/") { return DECLINED; }
   #We pretend to know how to handle files that actually exist!!
   if (!$dbh) { $dbh = &initialise($r); }
   my $cookie = undef;
