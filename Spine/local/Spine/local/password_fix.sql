7bbc211d4a5ac8d667828085dd52dfde603d7300
beatnik <beatnik@7e66c766-c70f-4bca-b50f-0f7bb70c072f>
2007-05-01 16:03:53 +0000

    Oops
    git-svn-id: svn+ssh://192.168.0.197/Users/hendrik/Repositories@195 7e66c766-c70f-4bca-b50f-0f7bb70c072f

--- Spine/local/Spine/local/password_fix.sql
+++
@@ -1,3 +0,0 @@
-delete from users where login = "admin";
-insert into users (email,fullname,login,password,usergroup) values ('root@localhost','Administrator','admin','acbd18db4cc2f85cedef654fccc4a4d8','admin'); 
-delete from session;
